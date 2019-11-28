8134bbbfadc65c14b9b7386ffb2d108ff1dda42b
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-11-28 11:31:42 +0530

    test

--- Project1/lambda.js
+++ Project1/lambda.js
@@ -1,4 +1,12 @@
 exports.handler = function(event, context, callback) {
+    var res = {
+  "isBase64Encoded": true,
+  "statusCode": 200,
+  "headers": {
+    "headerName": "headerValue"
+  },
+  "body": "..."
+}
     
-    callback(null, {"message": "Successfully executed"});
+    callback(null, {"message": res});
 }
